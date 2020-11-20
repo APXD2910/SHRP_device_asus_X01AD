e28154bdc6b9b07ea082fb603e21dc38e6a20819
丂P⚡乂D <somnatypaul007@gmail.com>
2020-11-21 02:56:10 +0530

    added builds vars

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,33 @@
-#
-# Copyright (C) 2018 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-add_lunch_combo omni_X01AD-userdebug
-add_lunch_combo omni_X01AD-eng
+
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_R11=1
+
+
+export ALLOW_MISSING_DEPENDENCIES=true
+
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_FLASHLIGHT_ENABLE=1
+#for 19:9
+export OF_SCREEN_H=2280
+export OF_FL_PATH1="/sys/class/leds/led:torch_0"
+export OF_FL_PATH2="/sys/class/leds/led:torch_1"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export OF_USE_TWRP_SAR_DETECT=1
+
+export OF_MAINTAINER="SP-XD (PIKA)"
+export LC_ALL="C"
+
+
+
+ #let's log what are the build VARs that we used
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "ALLOW" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "LC_" >> $FOX_BUILD_LOG_FILE
+  	fi
+
+
+
