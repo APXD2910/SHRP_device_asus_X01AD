95640bb503c155b14a95f3a98be03b8220ec8f5b
SP-XD <somnatypaul007@gmail.com>
2020-11-29 18:37:51 +0530

    added shrp flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,3 +129,30 @@ TW_INCLUDE_FUSE_NTFS := true
 
 #Ignore Missing Dependencies
 ALLOW_MISSING_DEPENDENCIES=true
+
+
+#SHRP VARIABLES 
+# Path of your SHRP Tree
+SHRP_PATH := device/asus/X01AD 
+# Maintainer name *
+SHRP_MAINTAINER := SP-XD 
+# Device codename *
+SHRP_DEVICE_CODE := X01AD 
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Treble
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A_Only
+
+SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_NOTCH := true
+SHRP_DARK := true
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 1
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/class/leds/led:torch_1/brightness
+SHRP_EDL_MODE := 1
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+
